f5dd5f86433878f5f9d28b2cff46826f791d479e
Gleb Khatkovskii <tosteg.gfmgf@gmail.com>
2023-07-20 19:15:07 +0400

    Corrections2

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -5,7 +5,6 @@ from django.urls import include, path
 from rest_framework import routers
 from cats.views import AchievementViewSet, CatViewSet
 
-
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
